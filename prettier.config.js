e38a635c0198f2288177963dbce76fcd16c83364
Victoria Rodriguez <vrodriguezfe@icloud.com>
2023-12-13 20:09:20 -0300

    chore: Init project

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+export default {
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  printWidth: 80,
+  proseWrap: "always",
+  quoteProps: "consistent",
+  useTabs: false,
+  endOfLine: "lf",
+  bracketSameLine: true,
+};
