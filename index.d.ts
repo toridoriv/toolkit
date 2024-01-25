670f0b67df636c384525f27d27a936f3006833ba
Victoria Rodriguez <vrodriguezfe@icloud.com>
2024-01-25 20:26:42 -0300

    fix(types): Improve declaration file to maintain documentation comments

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+export { constant, isDefined, isDefinedPrimitive, isNonNullable, isPrimitive, isUndefined, type Defined, type DefinedPrimitive, type Nullable, type Primitive } from "./lib/is.js";
+export { clean, getFirstChar, getLastChar, split, unwrap, wrap, type Clean, type GetFirstChar, type GetLastChar, type ReplaceAll, type Split, type Unwrap, type Wrap } from "./lib/string.js";
+export { Template, type AnyTemplate, type TemplateReplacements } from "./lib/template.js";
+
