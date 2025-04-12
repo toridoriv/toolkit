967f3020b7ab475e0139f43647abe37750434deb
Victoria Rodriguez <vrodriguezfe@icloud.com>
2025-04-12 17:49:35 -0400

    chore(deps): Update dependencies and upgrade settings accordingly

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,39 @@
 import { eslintConfig } from "@toridoriv/eslint-config";
 
-eslintConfig.ignorePatterns[0]?.ignores?.push("**/tmp/");
+/**
+ * @import {Linter} from "eslint"
+ */
 
 /**
- * @type {import("eslint").Linter.FlatConfig[]}
+ * Add or delete any Eslint configuration that you want (or not) to use in your project.
+ * You can import it from the eslint-config library or create your own.
+ *
+ * @type {Linter.Config[]}
  */
 export default [
-  ...eslintConfig.ignorePatterns,
+  {
+    ignores: [
+      "DS_Store",
+      "node_modules",
+      "package*.json",
+      "coverage",
+      "!bin/**",
+      "!.vscode/**",
+      "logs",
+      "*.log",
+      "npm-debug.log*",
+      "tmp/",
+      "*.tmp",
+      "tmp.*",
+      "var/tmp",
+      "bin/setup-eslint-config",
+    ],
+  },
   ...eslintConfig.javascript.node,
-  ...eslintConfig.typescript,
+  eslintConfig.typescript,
   ...eslintConfig.jsdoc,
-  ...eslintConfig.json,
-  ...eslintConfig.markdown,
-  ...eslintConfig.prettier,
+  eslintConfig.serialization.json,
+  eslintConfig.serialization.jsonc,
+  eslintConfig.markup.markdown,
+  eslintConfig.prettier,
 ];
